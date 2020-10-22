8e3b1138d7aa29b8138548df147017a746db7be1
mrdrivingduck <562655624@qq.com>
2020-10-22 10:40:43 +0800

    Raise exception to higher level

--- src/browser_operation.py
+++ src/browser_operation.py
@@ -17,110 +17,114 @@ def report_leave_school(config, timeout):
         return
 
     driver.get(url="https://ehall.nuaa.edu.cn/")
-    # time.sleep(timeout)
-    element = WebDriverWait(driver, timeout).until(
-        EC.presence_of_element_located((By.ID, 'username')))
-    element.send_keys(config["username"])
-    # login
-    # driver.find_element_by_id("username").send_keys(config["username"])
-    driver.find_element_by_id("password").send_keys(config["password"])
-    submit = driver.find_element_by_class_name("auth_login_btn")
-    submit.click()
-    time.sleep(1)
-    # time.sleep(timeout)
-    # select freedom service
-    request_page = WebDriverWait(driver, timeout).until(
-        EC.presence_of_element_located((By.LINK_TEXT, '疫情防控期学生出校申请/报备')))
-    # request_page = driver.find_element_by_link_text("疫情防控期学生出校申请/报备")
-    request_page.click()
-    time.sleep(8)
 
-    # open up the form
-    driver.switch_to.window(driver.window_handles[1])
-    form_page = WebDriverWait(driver, timeout).until(
-        EC.presence_of_element_located((By.ID, 'preview_start_button')))
-    # form_page = driver.find_element_by_id("preview_start_button")
-    form_page.click()
-    # time.sleep(timeout)
-
-    ####################### fill in the form ########################
-
-    if "assistant" in config:
-        time.sleep(8)
-        assistant = WebDriverWait(driver, timeout).until(
-            EC.presence_of_element_located((By.XPATH,
-                                            '/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[1]/table/tbody/tr[2]/td/div/table/tbody/tr[4]/td[2]/div/div/div/div/input')))
-        # assistant = driver.find_element_by_xpath(
-        # r'/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[1]/table/tbody/tr[2]/td/div/table/tbody/tr[4]/td[2]/div/div/div/div/input')
-        # assistant.click()
-        # assistant.send_keys(Keys.BACKSPACE)
-        # assistant.send_keys(Keys.BACKSPACE)
-
-        # 清空导员选择
-        driver.execute_script(
-            'document.querySelector(".xdTableContentRow > td > div > table > tbody > tr:nth-child(4) > td:nth-child(2) > div > div > div > div >input").value="";')
-
-        assistant.send_keys(config["assistant"])
+    try:
+        element = WebDriverWait(driver, timeout).until(
+            EC.presence_of_element_located((By.ID, 'username')))
+        element.send_keys(config["username"])
+        # login
+        # driver.find_element_by_id("username").send_keys(config["username"])
+        driver.find_element_by_id("password").send_keys(config["password"])
+        submit = driver.find_element_by_class_name("auth_login_btn")
+        submit.click()
+        time.sleep(1)
         # time.sleep(timeout)
+        # select freedom service
+        request_page = WebDriverWait(driver, timeout).until(
+            EC.presence_of_element_located((By.LINK_TEXT, '疫情防控期学生出校申请/报备')))
+        # request_page = driver.find_element_by_link_text("疫情防控期学生出校申请/报备")
+        request_page.click()
+        time.sleep(8)
+
+        # open up the form
+        driver.switch_to.window(driver.window_handles[1])
+        form_page = WebDriverWait(driver, timeout).until(
+            EC.presence_of_element_located((By.ID, 'preview_start_button')))
+        # form_page = driver.find_element_by_id("preview_start_button")
+        form_page.click()
+        # time.sleep(timeout)
+
+        ####################### fill in the form ########################
+
+        if "assistant" in config:
+            time.sleep(8)
+            assistant = WebDriverWait(driver, timeout).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                '/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[1]/table/tbody/tr[2]/td/div/table/tbody/tr[4]/td[2]/div/div/div/div/input')))
+            # assistant = driver.find_element_by_xpath(
+            # r'/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[1]/table/tbody/tr[2]/td/div/table/tbody/tr[4]/td[2]/div/div/div/div/input')
+            # assistant.click()
+            # assistant.send_keys(Keys.BACKSPACE)
+            # assistant.send_keys(Keys.BACKSPACE)
+
+            # 清空导员选择
+            driver.execute_script(
+                'document.querySelector(".xdTableContentRow > td > div > table > tbody > tr:nth-child(4) > td:nth-child(2) > div > div > div > div >input").value="";')
+
+            assistant.send_keys(config["assistant"])
+            # time.sleep(timeout)
+            time.sleep(2)
+            new_assistant = WebDriverWait(driver, timeout).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                '/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[1]/table/tbody/tr[2]/td/div/table/tbody/tr[4]/td[2]/div/div/div/div/div[1]/div')))
+            # new_assistant = driver.find_element_by_xpath(
+            #     r'/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[1]/table/tbody/tr[2]/td/div/table/tbody/tr[4]/td[2]/div/div/div/div/div[1]/div')
+            new_assistant.click()
+
+        if "supervisor" in config:
+            pass
+            # teacher = find_form.find_element_by_xpath(r'//tbody/tr[4]/td[4]/div/font/div/div/div/input')
+            # teacher.clear()
+            # teacher.send_keys('刘哲')
+        time.sleep(3)
+        temp = WebDriverWait(driver, timeout).until(
+            EC.presence_of_element_located((By.XPATH, r'//*[@id="V1_CTRL70"]')))
+        # campus = Select(driver.find_element_by_xpath(r'//*[@id="V1_CTRL70"]'))
+        campus = Select(temp)
+        campus.select_by_visible_text(config["campus"])
+
+        date = driver.find_element_by_xpath(r'//*[@id="V1_CTRL78"]')
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        date.clear()
+        date.send_keys(config["date"])
+
+        reason = driver.find_element_by_xpath(r'//*[@id="V1_CTRL80"]')
+        reason.click()
+        reason.clear()
+        reason.send_keys(config["reason"])
+
+        kind = driver.find_element_by_xpath(r'//*[@id="V1_CTRL107"]')
+        # kind.location_once_scrolled_into_view
+        kind.click()
+
+        is_leave = driver.find_element_by_xpath(r'//*[@id="V1_CTRL87"]')
+        # is_leave.location_once_scrolled_into_view
+        is_leave.click()
+
+        is_return = driver.find_element_by_xpath(r'//*[@id="V1_CTRL88"]')
+        # is_return.location_once_scrolled_into_view
+        is_return.click()
         time.sleep(2)
-        new_assistant = WebDriverWait(driver, timeout).until(
-            EC.presence_of_element_located((By.XPATH,
-                                            '/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[1]/table/tbody/tr[2]/td/div/table/tbody/tr[4]/td[2]/div/div/div/div/div[1]/div')))
-        # new_assistant = driver.find_element_by_xpath(
-        #     r'/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[1]/table/tbody/tr[2]/td/div/table/tbody/tr[4]/td[2]/div/div/div/div/div[1]/div')
-        new_assistant.click()
+        promise = driver.find_element_by_xpath(r'//*[@id="V1_CTRL90"]')
+        # ensure.location_once_scrolled_into_view
+        promise.click()
 
-    if "supervisor" in config:
-        pass
-        # teacher = find_form.find_element_by_xpath(r'//tbody/tr[4]/td[4]/div/font/div/div/div/input')
-        # teacher.clear()
-        # teacher.send_keys('刘哲')
-    time.sleep(3)
-    temp = WebDriverWait(driver, timeout).until(
-        EC.presence_of_element_located((By.XPATH, r'//*[@id="V1_CTRL70"]')))
-    # campus = Select(driver.find_element_by_xpath(r'//*[@id="V1_CTRL70"]'))
-    campus = Select(temp)
-    campus.select_by_visible_text(config["campus"])
+        submit_button = driver.find_element_by_xpath(
+            r'/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[3]/ul/li[1]/a')
+        submit_button.click()
 
-    date = driver.find_element_by_xpath(r'//*[@id="V1_CTRL78"]')
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    date.clear()
-    date.send_keys(config["date"])
+        # time.sleep(timeout)
+        final_check = WebDriverWait(driver, timeout).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[7]/div/div[2]/button[1]')))
 
-    reason = driver.find_element_by_xpath(r'//*[@id="V1_CTRL80"]')
-    reason.click()
-    reason.clear()
-    reason.send_keys(config["reason"])
-
-    kind = driver.find_element_by_xpath(r'//*[@id="V1_CTRL107"]')
-    # kind.location_once_scrolled_into_view
-    kind.click()
-
-    is_leave = driver.find_element_by_xpath(r'//*[@id="V1_CTRL87"]')
-    # is_leave.location_once_scrolled_into_view
-    is_leave.click()
-
-    is_return = driver.find_element_by_xpath(r'//*[@id="V1_CTRL88"]')
-    # is_return.location_once_scrolled_into_view
-    is_return.click()
-    time.sleep(2)
-    promise = driver.find_element_by_xpath(r'//*[@id="V1_CTRL90"]')
-    # ensure.location_once_scrolled_into_view
-    promise.click()
-
-    submit_button = driver.find_element_by_xpath(
-        r'/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[3]/ul/li[1]/a')
-    submit_button.click()
-
-    # time.sleep(timeout)
-    final_check = WebDriverWait(driver, timeout).until(
-        EC.presence_of_element_located((By.XPATH, '/html/body/div[7]/div/div[2]/button[1]')))
-
-    # final_check = driver.find_element_by_xpath(
-    #     r'/html/body/div[7]/div/div[2]/button[1]')
-    final_check.click()
-    driver.close()
-    driver.quit()
+        # final_check = driver.find_element_by_xpath(
+        #     r'/html/body/div[7]/div/div[2]/button[1]')
+        final_check.click()
+    except:
+        raise
+    finally:
+        driver.close()
+        driver.quit()
 
 
 def report_health_status(config, timeout):
